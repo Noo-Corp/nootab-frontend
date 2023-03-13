5fdaa201f5797f202e2d2ba305f276c00ca00593
Alex Moica <alex@alexmoica.com>
2023-03-13 02:16:22 -0400

    updated firefox extension v1.01

--- nootab_firefox_extension/nootab.js
+++ nootab_firefox_extension/nootab.js
@@ -1,11 +1,7 @@
 const url = "https://nootab.com/";
 
 async function setFocusOnUrl() {
-  const tabs = await browser.tabs.query({ active: true, currentWindow: true });
-  const tab = tabs[0];
-
-  await browser.tabs.create({ url, active: true, index: tab.index + 1 });
-  await browser.tabs.remove(tab.id);
+  await browser.tabs.update({ url });
 }
 
 setFocusOnUrl();
